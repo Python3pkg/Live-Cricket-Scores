8963ef5646fe1d3f9b5276bd043fd5420919d8f0
Python3pkg <raliclo@gmail.com>
2017-05-21 09:24:11 -0700

    Convert to Python3

--- Live_Cricket_Scores.py
+++ Live_Cricket_Scores.py
@@ -12,19 +12,19 @@ def getMatches(team = ''):
 	team_matches = []
 	
 	if team == '':
-		print "\nHere are the events going on live right now: "
+		print("\nHere are the events going on live right now: ")
 		for ix in range(0, len(tableHeads)):
-			print "\t" + str(ix+1) + ". " + str(tableHeads[ix].h2.text)
+			print("\t" + str(ix+1) + ". " + str(tableHeads[ix].h2.text))
 
 		try:
-			ch = raw_input("\nChoose the event for which you wish to check out the matches (Enter 0 to See All; -1 to exit): ")
+			ch = input("\nChoose the event for which you wish to check out the matches (Enter 0 to See All; -1 to exit): ")
 			ch = int(ch)
 			if ch == -1:
 				sys.exit("Hope you had fun. Have a great day ahead!")
 			temp = tableData[ch - 1] or (ch == 0)
 			
 		except (IndexError, ValueError):
-			print 'Please enter a valid integer between -1 and ' + str(len(tableData)) + '.'
+			print('Please enter a valid integer between -1 and ' + str(len(tableData)) + '.')
 			askForExit()
 	else:
 		ch = 0
@@ -61,21 +61,21 @@ def getMatches(team = ''):
 		
 		if team in ['', team1.lower(), team2.lower()]:
 			team_matches.append(ix+1)
-			print "\n" + headerline + "\t\t(" + str(matchDetails[0].find('span', {'class':'bold'}).text) +")"
-			print str(matchDetails[0].find('span', class_='match-no').a.text.split('     ',1)[1])
-			print "\t" + team1 + ": " + score1 + "\n\t" + team2 + ": " + score2
-			print "\n" + matchDetails[3].text.split('\n')[1]
-			print "_"*50
+			print("\n" + headerline + "\t\t(" + str(matchDetails[0].find('span', {'class':'bold'}).text) +")")
+			print(str(matchDetails[0].find('span', class_='match-no').a.text.split('     ',1)[1]))
+			print("\t" + team1 + ": " + score1 + "\n\t" + team2 + ": " + score2)
+			print("\n" + matchDetails[3].text.split('\n')[1])
+			print("_"*50)
 
 	if len(team_matches) == 0 and team != '':
-		print 'Sorry! No match found for team ' + team + '.'
+		print('Sorry! No match found for team ' + team + '.')
 		getMatches('')
 
 	try:
 		if len(team_matches) == 1:
 			ch = team_matches[0]
 		else:
-			ch = raw_input("\nChoose the event for which you wish to see the whole scorecard (Enter -1 to Exit; 0 for previous menu): ")
+			ch = input("\nChoose the event for which you wish to see the whole scorecard (Enter -1 to Exit; 0 for previous menu): ")
 			ch = int(ch)
 			if ch == -1:
 				sys.exit("Hope you had fun. Have a great day ahead!")
@@ -84,7 +84,7 @@ def getMatches(team = ''):
 			temp = matches[ch - 1]
 
 	except (IndexError, ValueError):
-		print 'Please enter a valid integer between -1 and ' + str(len(matches)) + '.'
+		print('Please enter a valid integer between -1 and ' + str(len(matches)) + '.')
 		askForExit()
 
 	url2 = "http://www.espncricinfo.com" + matches[ch-1].find_all('div')[4].find_all('a')[0]['href'] + "?view=scorecard"
@@ -110,7 +110,7 @@ def getMatches(team = ''):
 	askForExit()
 
 def askForExit():
-	ch = raw_input('\nEnter "y" if to check out more scores; any other input to exit: ')
+	ch = input('\nEnter "y" if to check out more scores; any other input to exit: ')
 	if ch.upper() in ['Y', 'YES', 'YA', 'YEAH', 'OUI', 'JA', 'HAAN', 'HA', 'SI']:
 		getMatches()
 	else:
@@ -137,7 +137,7 @@ def getBowlingTables(text):
 
 def getScoreCard(url, meta):
 
-	print "\n" + "*"*100
+	print("\n" + "*"*100)
 
 	r=requests.get(url)
 	soup=BS(r.text, "html.parser")
@@ -147,14 +147,14 @@ def getScoreCard(url, meta):
 
 	for ix in range(0, len(summary)-1):
 		summary[ix] = summary[ix].strip()
-		print summary[ix]
-	print '\n' + meta
+		print(summary[ix])
+	print('\n' + meta)
 
 	inningsBat = soup.find_all('table', {'class':'batting-table innings'})
 	inningsBowl = getBowlingTables(r.text)
 
 	for ix in range(0, len(inningsBat)):
-		print "\n" + "-" * 100
+		print("\n" + "-" * 100)
 		
 		rows = inningsBat[ix].find_all('tr')
 
@@ -167,7 +167,7 @@ def getScoreCard(url, meta):
 		inningHead = rows[0].find_all('th')[1].text.split('innings', 1)[0] + 'Innings'
 		if len(inningHead) < 60:
 			inningHead += " " * (60 - len(inningHead))
-		print "\n" + inningHead + "\t" + "Runs" + "\t"+ "Balls" + "\t" + "Strike Rate" + "\n"
+		print("\n" + inningHead + "\t" + "Runs" + "\t"+ "Balls" + "\t" + "Strike Rate" + "\n")
 
 		for px in range(1, len(rows)-2, 2):
 			playerData = rows[px].find_all('td')
@@ -183,7 +183,7 @@ def getScoreCard(url, meta):
 			balls = playerData[len(playerData) - 4].text
 			if len(balls) < 5:
 				balls += " " * (5 - len(balls))
-			print playerName + dismissalMode + "\t" + runs + "\t" + balls + "\t" + playerData[len(playerData)-1].text
+			print(playerName + dismissalMode + "\t" + runs + "\t" + balls + "\t" + playerData[len(playerData)-1].text)
 
 		playerData = rows[len(rows) - 2].find_all('td')
 		playerName = playerData[1].text
@@ -194,7 +194,7 @@ def getScoreCard(url, meta):
 		runs = playerData[3].text
 		if len(runs)<4:
 			runs += " "*(4-len(runs))
-		print "\n" + playerName + dismissalMode + "\t" + runs
+		print("\n" + playerName + dismissalMode + "\t" + runs)
 
 		playerData = rows[len(rows) - 1].find_all('td')
 		playerName = playerData[1].text
@@ -205,13 +205,13 @@ def getScoreCard(url, meta):
 		runs = playerData[3].text
 		if len(runs) < 4:
 			runs += " " * (4 - len(runs))
-		print playerName + dismissalMode + "\t" + runs + "\t" + playerData[4].text
+		print(playerName + dismissalMode + "\t" + runs + "\t" + playerData[4].text)
 
-		print "_" * 20
+		print("_" * 20)
 
 		inningHead = 'Bowling'
 		inningHead += " " * (30 - len(inningHead))
-		print "\n" + inningHead + "\t" + "Overs" + "\t" "Maidens" + "\t" + "Runs" + "\t" + "Wickets" + "\t" + "Economy" + "\n"
+		print("\n" + inningHead + "\t" + "Overs" + "\t" "Maidens" + "\t" + "Runs" + "\t" + "Wickets" + "\t" + "Economy" + "\n")
 		
 		for row in range(len(inningsBowl[ix])):
 			playerName = str(inningsBowl[ix][row][0])
@@ -230,12 +230,12 @@ def getScoreCard(url, meta):
 			if len(wickets)<7:
 				wickets += " "*(7-len(wickets))
 			economy = str(inningsBowl[ix][row][5])
-			print playerName + "\t" + overs + "\t" + maidens + "\t" + runs + "\t" + wickets + "\t" + economy
+			print(playerName + "\t" + overs + "\t" + maidens + "\t" + runs + "\t" + wickets + "\t" + economy)
 
 if __name__ == '__main__':
 	sys.stdout.write("\x1b[8;{rows};{cols}t".format(rows=50, cols=100))
-	print "\n" + "*" *100
-	print "Welcome to Live Cricket Scores Terminal App v2.0 by Navjot Singh"
+	print("\n" + "*" *100)
+	print("Welcome to Live Cricket Scores Terminal App v2.0 by Navjot Singh")
 	
 	team = ''
 	for ix in range(1, len(sys.argv)):
